c04e408ad27d5f1b2bf12f40c0d26548d5004311
Gryffon Bellish <owenbellish@gmail.com>
2022-08-20 07:15:07 -0400

    more work

--- src/SimpleButton.tsx
+++ src/SimpleButton.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 
-// export function Button() {
-//     return (<button onClick={getApps}>Get Apps</button>)
-// }
-
-// async function getApps(): Promise<string> {
-//     const p = await invoke('get_bungie_applications') as object;
-
-//     return JSON.stringify(p);
-// }
-
 export class SimpleButton extends React.Component<{}, { data: string, fetched: boolean }> {
     public state = { data: "", fetched: false };
 
     public render(): React.ReactNode {
-        // return (
-        //     <button onClick={async () => await this.fetch()}>
-        //         {this.state.data}
-        //     </button>
-        // )
         return (
             <div>
                 <button onClick={async () => await this.fetch()}>Click me!</button>
